072d476fcf6de79b4fa066d98cc0e41e469b676a
Eric Tang <etang@college.harvard.edu>
2022-01-07 13:16:07 -0500

    Updates

---
+++ precache-manifest.9d878570522db7a5b8f12a9b32d761ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719384dc6c66539fc2c19601aa927856",
+    "url": "/personal-assistant/index.html"
+  },
+  {
+    "revision": "b6b0dc7fcd7a7ffe3526",
+    "url": "/personal-assistant/static/js/2.2acf81d9.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/personal-assistant/static/js/2.2acf81d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e089cb49822d272157",
+    "url": "/personal-assistant/static/js/main.9b0e5d2f.chunk.js"
+  },
+  {
+    "revision": "b8b8e24a3d2cb0a90037",
+    "url": "/personal-assistant/static/js/runtime-main.c67efb09.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.517b3887e4b0fa913216cc71fd91c89c.js"
+  "/personal-assistant/precache-manifest.9d878570522db7a5b8f12a9b32d761ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-assistant/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
